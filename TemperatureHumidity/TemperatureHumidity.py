8bcf253fefc091d529a14d4ba0569925fb4a9b2d
zona8815 <emmachu68@gmail.com>
2021-08-01 17:12:25 +0800

    test for TemperatureHumidity.py

--- TemperatureHumidity/TemperatureHumidity.py
+++ TemperatureHumidity/TemperatureHumidity.py
@@ -1,10 +1,19 @@
+import board
+import busio
+import adafruit_sht31d
+import time
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensorSHT31 = adafruit_sht31d.SHT31D(i2c, 0x44)
 
 class TemperatureHumidity:
     def __init__(self):
         return
 
     def getTemperatureData(self):
-        return 100
+        print('Temp = {:.1f} \u00b0C'.format(sensorSHT31.temperature))
+        return
 
     def getHumidityData(self):
-        return 100
+        print('RH = {:.1f} %\n'.format(sensorSHT31.relative_humidity))
+        return 50
