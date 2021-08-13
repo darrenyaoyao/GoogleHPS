a6d7c56d12f6feec656fb8633da5d5fa02f0ebe3
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-13 15:26:22 +0800

    Update AirQuality.py

--- AirQuality/AirQuality.py
+++ AirQuality/AirQuality.py
@@ -17,12 +17,22 @@ class AirQuality:
         t = int(self.sht31d.temperature)
         AH = 216.7 * ((RH / 100.0) * 6.112 * math.exp((17.62 * t) / (243.12 + t)) / (273.15 + t))
         self.sgp30.set_iaq_humidity(AH)
+        
+        self.last_read_eCO2 = 0
+        self.last_read_eCO2_time = 0
+        self.last_read_TVOC = 0
+        self.last_read_TVOC_time = 0
+        
         return
     
     def get_eCO2_Data(self):
+        self.last_read_eCO2 += self.sgp30.eCO2
+        self.last_read_eCO2_time += 1
         return self.sgp30.eCO2
     
     def get_TVOC_Data(self):
+        self.last_read_TVOC += self.sgp30.TVOC
+        self.last_read_TVOC_time += 1
         return self.sgp30.TVOC
     
     def get_Ethanol_Data(self):
@@ -34,10 +44,15 @@ class AirQuality:
     def good_or_bad(self):
         # CO₂ standard：average 1000 ppm within 8 hours
         # TVOC standard：average 0.56 ppm within 1 hours
-        if self.sgp30.eCO2 <= 1000 and self.sgp30.TVOC*1000 <= 0.56 :
+        self.last_read_eCO2 += self.sgp30.eCO2
+        self.last_read_eCO2_time += 1
+        self.last_read_TVOC += self.sgp30.TVOC
+        self.last_read_TVOC_time += 1
+        
+        if self.last_read_eCO2/self.last_read_eCO2_time <= 1000 and self.last_read_TVOC/self.last_read_TVOC_time*1000 <= 0.56 :
             return "Can't be better！"
-        elif self.sgp30.eCO2 > 1000 and self.sgp30.TVOC*1000 <= 0.56 :
+        elif self.last_read_eCO2/self.last_read_eCO2_time > 1000 and self.last_read_TVOC/self.last_read_TVOC_time*1000 <= 0.56 :
             return 'It seems that CO₂ is too high！'
-        elif self.sgp30.eCO2 <= 1000 and self.sgp30.TVOC*1000 > 0.56 :
+        elif self.last_read_eCO2/self.last_read_eCO2_time <= 1000 and self.last_read_TVOC/self.last_read_TVOC_time*1000 > 0.56 :
             return 'It seems that TVOC is too high！'
         else : return 'Both CO₂ and TVOC are too high！So bad......'
