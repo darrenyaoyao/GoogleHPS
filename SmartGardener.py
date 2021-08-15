3985bbabd03610286bb8ade1610dabd2f905a278
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-16 05:40:34 +0800

    Update SmartGardener.py

--- SmartGardener.py
+++ SmartGardener.py
@@ -28,6 +28,7 @@ def home():
             coloe_lux=str(light.get_color_temperature_Data()),
             cold_or_hot=temperatureHumidity.cold_or_hot(),
             temperature=str(temperatureHumidity.getTemperatureData()),
+            dry_or_wet=temperatureHumidity.dry_or_wet(),
             humidity=str(temperatureHumidity.getHumidityData()),
             watering=str(watering.getWateringData()))
 
