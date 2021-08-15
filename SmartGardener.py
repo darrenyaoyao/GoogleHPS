843777db7542384a4fb911b93fe0ab5f2207bfaa
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-16 04:14:24 +0800

    Update SmartGardener.py

--- SmartGardener.py
+++ SmartGardener.py
@@ -24,6 +24,7 @@ def home():
             TVOC_Data=airQuality.get_TVOC_Data(),
             gsensor_data=str(gsensor.getData()),
             light=str(light.getData()),
+            coloe_lux=str(get_color_temperature_Data()),
             temperature=str(temperatureHumidity.getTemperatureData()),
             humidity=str(temperatureHumidity.getHumidityData()),
             watering=str(watering.getWateringData()))
