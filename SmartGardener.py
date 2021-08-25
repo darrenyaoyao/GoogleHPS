8c3d49126b03cf3bbdf185c46c6dc5a150f698b7
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-25 23:14:03 +0800

    Update SmartGardener.py

--- SmartGardener.py
+++ SmartGardener.py
@@ -4,6 +4,7 @@ from GSensor.GSensor import GSensor
 from Light.Light import Light
 from TemperatureHumidity.TemperatureHumidity import TemperatureHumidity
 import RPi.GPIO as GPIO
+import datetime
 import time
 
 app = Flask("Smart Gardener")
@@ -32,20 +33,27 @@ def home():
             p.stop()
         elif request.form.get('Watering') == 'Watering':
             print('Watering')
-    return render_template('home.html', airQuality_good_or_bad=airQuality.good_or_bad(),
-            eCO2_Data=airQuality.get_eCO2_Data(),
-            TVOC_Data=airQuality.get_TVOC_Data(),
-            gsensor_data=str(gsensor.getData()),
-            stable_or_sway=gsensor.stable_or_sway(),
-            bright_ot_dark=light.bright_ot_dark(),
-            light_value=light.getData(),
-            coloe_lux=str(light.get_color_temperature_Data()),
-            cold_or_hot=temperatureHumidity.cold_or_hot(),
-            temperature=str(temperatureHumidity.getTemperatureData()),
-            temperatureF=str(temperatureHumidity.getTemperatureF()),
-            dry_or_wet=temperatureHumidity.dry_or_wet(),
-            dry_or_wet_people=temperatureHumidity.dry_or_wet_people(),
-            humidity=str(temperatureHumidity.getHumidityData()),)
+    return render_template('home.html',
+                           airQuality_good_or_bad=airQuality.good_or_bad(),
+                           eCO2_Data=airQuality.get_eCO2_Data(),
+                           TVOC_Data=airQuality.get_TVOC_Data(),
+                           gsensor_data=str(gsensor.getData()),
+                           stable_or_sway=gsensor.stable_or_sway(),
+                           bright_ot_dark=light.bright_ot_dark(),
+                           light_value=light.getData(),
+                           coloe_lux=str(light.get_color_temperature_Data()),
+                           cold_or_hot=temperatureHumidity.cold_or_hot(),
+                           temperature=str(temperatureHumidity.getTemperatureData()),
+                           temperatureF=str(temperatureHumidity.getTemperatureF()),
+                           dry_or_wet=temperatureHumidity.dry_or_wet(),
+                           dry_or_wet_people=temperatureHumidity.dry_or_wet_people(),
+                           humidity=str(temperatureHumidity.getHumidityData()),
+                           year=datetime.datetime.now().year,
+                           month=datetime.datetime.now().month,
+                           day=datetime.datetime.now().day,
+                           hour=datetime.datetime.now().hour,
+                           minute=datetime.datetime.now().minute,
+                          )
 
 if __name__ == '__main__':
    app.run(debug=True, port=80, host='0.0.0.0')
