89a101bd1a93b9c1140b8af049fb593f32a2921f
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-26 16:04:05 +0800

    Update time.py

--- Time/time.py
+++ Time/time.py
@@ -38,6 +38,12 @@ class timeInfo:
     def get_last_watering_time(self):
         return str(self.waterMonth) + '月' + str(self.waterDay) + '日 ' + str(self.waterHour).zfill(2) + ':' + str(self.waterMinute).zfill(2)
     
+    def upon_last_watering_time(self):
+        minute = self.waterHour * 60 + self.waterMinute - datetime.datetime.now().minute *60 - datetime.datetime.now().minute
+        hour = minute / 60
+        minute %= 60
+        return str(hour) + ':' + str(minute)
+    
     def update_last_noise_year(self,year):
         self.noiseYear = year
         
@@ -61,3 +67,9 @@ class timeInfo:
     
     def get_last_noise_time(self):
         return str(self.noiseMonth) + '月' + str(self.noiseDay) + '日 ' + str(self.noiseHour).zfill(2) + ':' + str(self.noiseMinute).zfill(2)
+
+    def upon_last_noise_time(self):
+        minute = self.noiseHour * 60 + self.noiseMinute - datetime.datetime.now().minute *60 - datetime.datetime.now().minute
+        hour = minute / 60
+        minute %= 60
+        return str(hour) + ':' + str(minute)
