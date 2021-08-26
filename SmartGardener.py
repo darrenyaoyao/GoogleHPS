a8fcbc253ed11f3d27bdd0c6801464e6dc133af3
AmyCHANGY <88243326+AmyCHANGY@users.noreply.github.com>
2021-08-26 18:27:16 +0800

    Update SmartGardener.py

--- SmartGardener.py
+++ SmartGardener.py
@@ -40,6 +40,7 @@ def home():
             p.ChangeFrequency(659)
             time.sleep(1)
             p.stop()
+            GPIO.output(BUZZIER, GPIO.HIGH）
         elif request.form.get('Watering') == 'Watering':
                timeInfo.update_last_watering_year(str(datetime.datetime.now().year))
                timeInfo.update_last_watering_month(datetime.datetime.now().month)
