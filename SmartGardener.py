188634bbb807d48395258d48c4ba5b38643a11d0
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-26 14:47:29 +0800

    Update SmartGardener.py

--- SmartGardener.py
+++ SmartGardener.py
@@ -45,7 +45,7 @@ def home():
                timeInfo.update_last_watering_hour(datetime.datetime.now().hour)
                timeInfo.update_last_watering_minute(datetime.datetime.now().minute)
                print('Watering')
-    if temperatureHumidity.auto_water() == true :
+    if temperatureHumidity.auto_water() == 'true' :
        if datetime.datetime.now().hour * 60 + datetime.datetime.now().minute - timeInfo.get_last_watering_hour() * 60 - timeInfo.get_last_watering_minute() >= 720 :
           timeInfo.update_last_watering_year(str(datetime.datetime.now().year))
           timeInfo.update_last_watering_month(datetime.datetime.now().month)
