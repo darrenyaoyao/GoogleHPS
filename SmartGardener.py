385773b82f4c0bb20c90e69ced2200d7c31a42c9
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-26 17:38:41 +0800

    Update SmartGardener.py

--- SmartGardener.py
+++ SmartGardener.py
@@ -85,6 +85,45 @@ def home():
                                   upon_last_watering_time=timeInfo.upon_last_watering_time(),
                                   upon_last_noise_time=timeInfo.upon_last_noise_time(),
                                   )
+    if gsensor.auto_noise() == 'true' :
+        p = GPIO.PWM(BUZZIER, 50)
+        p.start(50)
+        p.ChangeFrequency(523)
+        time.sleep(1)
+        p.ChangeFrequency(587)
+        time.sleep(1)
+        p.ChangeFrequency(659)
+        time.sleep(1)
+        p.stop()
+        timeInfo.update_last_noise_month(datetime.datetime.now().month)
+        timeInfo.update_last_noise_day(datetime.datetime.now().day)
+        timeInfo.update_last_noise_hour(datetime.datetime.now().hour)
+        timeInfo.update_last_noise_minute(datetime.datetime.now().minute)
+        return render_template('home.html',
+                                  airQuality_good_or_bad=airQuality.good_or_bad(),
+                                  eCO2_Data=airQuality.get_eCO2_Data(),
+                                  TVOC_Data=airQuality.get_TVOC_Data(),
+                                  gsensor_data=str(gsensor.getData()),
+                                  stable_or_sway=gsensor.stable_or_sway(),
+                                  bright_ot_dark=light.bright_ot_dark(),
+                                  light_value=light.getData(),
+                                  coloe_lux=str(light.get_color_temperature_Data()),
+                                  cold_or_hot=temperatureHumidity.cold_or_hot(),
+                                  temperature=str(temperatureHumidity.getTemperatureData()),
+                                  temperatureF=str(temperatureHumidity.getTemperatureF()),
+                                  dry_or_wet=temperatureHumidity.dry_or_wet(),
+                                  dry_or_wet_people=temperatureHumidity.dry_or_wet_people(),
+                                  humidity=str(temperatureHumidity.getHumidityData()),
+                                  year=datetime.datetime.now().year,
+                                  month=datetime.datetime.now().month,
+                                  day=datetime.datetime.now().day,
+                                  hour=str(datetime.datetime.now().hour).zfill(2),
+                                  minute=str(datetime.datetime.now().minute).zfill(2),
+                                  last_water_time=timeInfo.get_last_watering_time(),
+                                  last_noise_time=timeInfo.get_last_noise_time(),
+                                  upon_last_watering_time=timeInfo.upon_last_watering_time(),
+                                  upon_last_noise_time=timeInfo.upon_last_noise_time(),
+                                  )
     return render_template('home.html',
                            airQuality_good_or_bad=airQuality.good_or_bad(),
                            eCO2_Data=airQuality.get_eCO2_Data(),
