ecb113382b0de54c429e99bfa14e561096df96ef
Ricky Chuang <songsongyyohya@gmail.com>
2021-08-27 21:31:06 +0800

    Update GSensor.py

--- GSensor/GSensor.py
+++ GSensor/GSensor.py
@@ -22,7 +22,9 @@ class GSensor:
 		# print('Raw ADC Value: ', self.chan0.value)
 		# print('ADC Voltage: ' + str(self.chan0.voltage) + 'V')
 		self.last_read = 0
+		self.last_read_y = 0
 		self.tolerance = 250
+		self.tolerance_y = 250
 
 	def remap_range(self, value, left_min, left_max, right_min, right_max):
 		# this remaps a value from original (left) range to new (right) range
@@ -63,6 +65,23 @@ class GSensor:
 			return set_volume
 		else : return self.remap_range(self.last_read, 0, 65535, 0, 100)
 
+	def getDataY(self):
+		trim_pot_changed = False
+		trim_pot = self.chan1.value
+		pot_adjust = abs(trim_pot - self.last_read_y)
+
+		if pot_adjust > self.tolerance_y:
+			trim_pot_changed = True
+
+		if trim_pot_changed:
+			set_volume = self.remap_range(trim_pot, 0, 65535, 0, 100)
+			set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' \
+			.format(volume = set_volume)
+			os.system(set_vol_cmd)
+			self.last_read_y = trim_pot
+			return set_volume
+		else : return self.remap_range(self.last_read_y, 0, 65535, 0, 100)
+		
 	def stable_or_sway(self):
 		trim_pot_changed = False
 		trim_pot = self.chan0.value
